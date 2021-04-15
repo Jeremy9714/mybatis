cfe898cc0a761206a3f1bb9d496b04139a887d8c
CYZ <1822550890@qq.com>
2021-04-15 14:47:11 +0100

    映射文件

---
+++ mybatis/MyBatis_03_mapper/src/main/java/com/jeremy/bean/Employee.java
@@ -0,0 +1,59 @@
+package com.jeremy.bean;
+
+import org.apache.ibatis.type.Alias;
+
+/**
+ * @author Chenyang
+ * @create 2021-04-14-19:17
+ */
+
+@Alias("emp") //批量起别名的情况下，可以通过Alias注解可以给类型起新的别名
+public class Employee {
+
+    private int id;
+    private String lastName;
+    private String gender;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
