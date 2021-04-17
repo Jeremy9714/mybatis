abcb76b06884ac889643dcad08f1f68c102e5c0f
CYZ <1822550890@qq.com>
2021-04-17 18:48:32 +0100

    log4j日志

---
+++ mybatis/MyBatis_05_cache/src/test/java/com/jeremy/CacheTest.java
@@ -0,0 +1,40 @@
+package com.jeremy;
+
+import com.jeremy.bean.Employee;
+import com.jeremy.dao.EmployeeMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author Chenyang
+ * @create 2021-04-17-17:02
+ */
+public class CacheTest {
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        String resource = "mybatis-config.xml";
+        try {
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test1() {
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            EmployeeMapper mapper = session.getMapper(EmployeeMapper.class);
+            Employee emp = mapper.getEmp(1);
+            System.out.println(emp);
+        }
+    }
+}
