98a1db0f524ba2832ca9ea3efaf8334f1721e656
CYZ <1822550890@qq.com>
2021-04-17 19:44:34 +0100

    二级缓存的使用

--- mybatis/MyBatis_05_cache/src/test/java/com/jeremy/CacheTest.java
+++ mybatis/MyBatis_05_cache/src/test/java/com/jeremy/CacheTest.java
@@ -29,6 +29,7 @@ public class CacheTest {
         }
     }
 
+    //一级缓存
     @Test
     public void testFirstLevelCache() {
         SqlSession session1 = sqlSessionFactory.openSession();
@@ -42,15 +43,34 @@ public class CacheTest {
             Employee emp2 = mapper2.getEmp(1);
             System.out.println(emp2);
 
-            System.out.println(emp1==emp2);
-        }finally{
+            System.out.println(emp1 == emp2);
+        } finally {
             session1.close();
             session2.close();
         }
     }
 
+    //二级缓存
     @Test
-    public void testSecondLevelCache(){
+    public void testSecondLevelCache() {
+        SqlSession session1 = sqlSessionFactory.openSession();
+        SqlSession session2 = sqlSessionFactory.openSession();
+        try {
+            EmployeeMapper mapper1 = session1.getMapper(EmployeeMapper.class);
+            EmployeeMapper mapper2 = session2.getMapper(EmployeeMapper.class);
 
+            Employee emp1 = mapper1.getEmp(1);
+            System.out.println(emp1);
+            session1.close();
+
+            //第二次查询，是从二级缓存中获取的数据，并未发送新的sql
+            Employee emp2 = mapper2.getEmp(1);
+            System.out.println(emp2);
+            session2.close();
+
+            System.out.println(emp1 == emp2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
