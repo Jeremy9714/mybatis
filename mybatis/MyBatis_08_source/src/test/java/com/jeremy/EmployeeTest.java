9114d1cfb1b4833a553bd0febd72753663666ac2
CYZ <1822550890@qq.com>
2021-04-19 15:16:42 +0100

    MyBatis运行原理

--- mybatis/MyBatis_08_source/src/test/java/com/jeremy/EmployeeTest.java
+++ mybatis/MyBatis_08_source/src/test/java/com/jeremy/EmployeeTest.java
@@ -19,11 +19,22 @@ public class EmployeeTest {
 
     @Test
     public void test1() throws IOException {
-        //根据全局配置文件得到SqlSessionFactory对象
+        /**
+         * 解析配置文件的每一个信息并保存到Configuration中，返回包含Configuration的DefaultSqlSession
+         *      Configuration中的MapperStatement代表一个增删改查标签的全部信息
+         */
         SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
 
-        //获取SqlSession实例，可以直接执行已经映射的sql语句
+        /**
+         * 返回一个DefaultSqlSession对象，包含Executor和Configuration
+     *          在这一步中会创建Executor对象，并用每一个拦截器来包装该Executor对象
+         */
         try (SqlSession session = sqlSessionFactory.openSession()) {
+
+            /**
+             * getMapper方法中，会根据接口类型从KnownMapper中获取MapperProxyFactory
+             *      通过MApperProxyFactory创建MapperProxy代理对象, 该对象包含了DefaultSqlSession对象
+             */
             EmployeeMapper mapper = session.getMapper(EmployeeMapper.class);
             Employee employee = mapper.getEmployee(1);
             System.out.println(mapper.getClass());
@@ -35,6 +46,7 @@ public class EmployeeTest {
         //全局配置文件的路径
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
+
         return new SqlSessionFactoryBuilder().build(inputStream);
     }
 }
